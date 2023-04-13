ee8da24fe9cf71d88e13f3ae691c5f3b5c8bcd2c
Popitopi130 <j-emmanuel@hotmail.ca>
2023-04-12 20:00:51 -0400

    Tâche 0 : JavaDoc du package server
    Classes:
    - ServerLauncher
    - Server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,3 +1,7 @@
+/**
+ * Auteurs: Oussama Ben Sghaier et Simon Darveau
+ */
+
 package server;
 
 import javafx.util.Pair;
@@ -16,9 +20,22 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Classe qui représente le serveur. S'occupe de gérer les requêtes du client et d'exécuter des méthodes suite à
+ * l'obtention de commande et d'argument.
+ * <p>Les commandes sont : </p>
+ * - INSCRIRE: Permet au client de s'inscrire à un cours <br>
+ * - CHARGER: Permet au client de récupérer la liste de cours pour une session donnée.
+ */
 public class Server {
 
+    /**
+     * La commande qui permet au client de s'inscrire à un cours
+     */
     public final static String REGISTER_COMMAND = "INSCRIRE";
+    /**
+     * La commande qui permet au client de charger la liste de cours pour une session donnée
+     */
     public final static String LOAD_COMMAND = "CHARGER";
     private final ServerSocket server;
     private Socket client;
@@ -26,12 +43,21 @@ public class Server {
     private ObjectOutputStream objectOutputStream;
     private final ArrayList<EventHandler> handlers;
 
+    /**
+     * Instancie le serveur (constructor).
+     * @param port int le numéro du port auquel se connecter
+     * @throws IOException
+     */
     public Server(int port) throws IOException {
         this.server = new ServerSocket(port, 1);
         this.handlers = new ArrayList<EventHandler>();
         this.addEventHandler(this::handleEvents);
     }
 
+    /**
+     * Permet d'ajouter un évènement à la liste d'EventHandler
+     * @param h - EventHandler à ajouter à la liste d'EventHandler
+     */
     public void addEventHandler(EventHandler h) {
         this.handlers.add(h);
     }
@@ -42,6 +68,9 @@ public class Server {
         }
     }
 
+    /**
+     * Démarre et fait fonctionner le server.
+     */
     public void run() {
         while (true) {
             try {
@@ -58,6 +87,11 @@ public class Server {
         }
     }
 
+    /**
+     * Permet au serveur d'écouter les requêtes du client
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public void listen() throws IOException, ClassNotFoundException {
         String line;
         if ((line = this.objectInputStream.readObject().toString()) != null) {
@@ -68,6 +102,11 @@ public class Server {
         }
     }
 
+    /**
+     * Sépare la commande et l'argument envoyer au serveur.
+     * @param line String : la commande et l'argument
+     * @return objet Pair contenant la commande à exécuter (String) et les arguments de la commande (String)
+     */
     public Pair<String, String> processCommandLine(String line) {
         String[] parts = line.split(" ");
         String cmd = parts[0];
@@ -75,12 +114,21 @@ public class Server {
         return new Pair<>(cmd, args);
     }
 
+    /**
+     * Déconnecte le client du serveur et ferme les streams d'imput et d'output
+     * @throws IOException
+     */
     public void disconnect() throws IOException {
         objectOutputStream.close();
         objectInputStream.close();
         client.close();
     }
 
+    /**
+     * Permet au serveur de gérer et d'exécuter les commandes qui lui sont envoyées par le client
+     * @param cmd le nom de la commande a exécuter
+     * @param arg utiliser par la commande "charger": la session pour laquelle on veut récupérer la liste des cours
+     */
     public void handleEvents(String cmd, String arg) {
         if (cmd.equals(REGISTER_COMMAND)) {
             handleRegistration();
@@ -95,6 +143,7 @@ public class Server {
      Ensuite, elle renvoie la liste des cours pour une session au client en utilisant l'objet 'objectOutputStream'.
      La méthode gère les exceptions si une erreur se produit lors de la lecture du fichier ou de l'écriture de l'objet dans le flux.
      @param arg la session pour laquelle on veut récupérer la liste des cours
+     @throws IOException
      */
     public void handleLoadCourses(String arg) {
         ArrayList<Course> listeDeCours = new ArrayList<>();
@@ -129,6 +178,8 @@ public class Server {
      Récupérer l'objet 'RegistrationForm' envoyé par le client en utilisant 'objectInputStream', l'enregistrer dans un fichier texte
      et renvoyer un message de confirmation au client.
      La méthode gére les exceptions si une erreur se produit lors de la lecture de l'objet, l'écriture dans un fichier ou dans le flux de sortie.
+     @throws IOException
+     @throws ClassNotFoundException
      */
     public void handleRegistration() {
         try {
--- src/main/java/server/ServerLauncher.java
+++ src/main/java/server/ServerLauncher.java
@@ -1,8 +1,22 @@
+/**
+ * Auteur: Oussama Ben Sghaier
+ */
+
 package server;
 
+/**
+ * Permet de lancer un serveur avec un port spécifique.
+ */
 public class ServerLauncher {
+    /**
+     * Le numéro de port sur lequel un client peut envoyer des requêtes
+     */
     public final static int PORT = 1337;
 
+    /**
+     * Crée et démarre le serveur
+     * @param args String[]
+     */
     public static void main(String[] args) {
         Server server;
         try {
